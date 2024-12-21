a9ebf0a35b65c308531590e0956ef638c6ae3b2f
Kenson <city252@qq.com>
2024-12-21 17:12:47 +0800

    路由组件的基本使用2

--- react_staging/src/App.js
+++ react_staging/src/App.js
@@ -1,18 +1,10 @@
 import React, { Component } from 'react'
-import { Route } from 'react-router-dom'
-import TopBar from './compoents/TopBar'
-import Home from './compoents/Home'
-import About from './compoents/About'
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <TopBar />
-        {/* 当前url为 /home时显示 Home组件 */}
-        <Route path="/home" component={Home} />
-        {/* 当前url为 /about时显示 About组件 */}
-        <Route path="/about" component={About} />
+        APP...
       </div>
     )
   }
--- react_staging/src/index.js
+++ react_staging/src/index.js
@@ -1,13 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { BrowserRouter } from 'react-router-dom'
 
-// 由于一个BrowserRouter只能管理被其包裹的路由，所以，在App外侧使用一个BrowserRouter包裹整个应用，这样，整个应用都被该路由管理
-// 注意：BrowserRouter组件中不能再嵌套一个BrowserRouter
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+    <App />
 )
