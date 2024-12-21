6c569530516e304dd2941731c71c80cc779d0d51
Kenson <city252@qq.com>
2024-12-21 21:01:25 +0800

    Switch的使用

--- react_staging/05_路由组件的基本使用_src/App.js
+++ react_staging/05_路由组件的基本使用_src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Route } from 'react-router-dom'
+import { Route,Switch } from 'react-router-dom'
 import TopBar from './compoents/TopBar'// 一般组件
 import Home from './pages/Home'// 路由组件
 import About from './pages/About'// 路由组件
@@ -10,9 +10,19 @@ export default class App extends Component {
       <div>
         <TopBar />
         {/* 当前url为 /home时显示 Home组件 */}
-        <Route path="/home" component={Home} />
+        {/* <Route path="/home" component={Home} /> */}
         {/* 当前url为 /about时显示 About组件 */}
-        <Route path="/about" component={About} />
+        {/* <Route path="/about" component={About} /> */}
+
+        {/* 如果同一个路由配置了多个组件，那么这些组件会同时展示 */}
+        {/* <Route path="/home" component={Home} />
+        <Route path="/home" component={About} /> */}
+
+        {/* 如果外层包裹了Switch组件，那么只会渲染第一个匹配到的组件 */}
+        <Switch>
+          <Route path="/home" component={Home} />
+          <Route path="/home" component={About} />
+        </Switch>
       </div>
     )
   }
