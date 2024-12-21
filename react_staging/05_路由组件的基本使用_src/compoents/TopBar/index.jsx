084e59ebd0e6614ee9ecdc0435248f7b92f748b7
Kenson <city252@qq.com>
2024-12-21 17:53:48 +0800

    NavLink的使用

--- react_staging/05_路由组件的基本使用_src/compoents/TopBar/index.jsx
+++ react_staging/05_路由组件的基本使用_src/compoents/TopBar/index.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './TopBar.css'; // 引入CSS文件
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 export default class TopBar extends Component {
     render() {
@@ -9,9 +9,10 @@ export default class TopBar extends Component {
                 <nav className="nav-links">
                     <ul>
                         {/* 点击时跳转到/home路由 */}
-                        <li><Link to="/home">首页</Link></li>
+                        {/* 使用NavLink组件给组件添加点击后的效果,可以添加activeClassName属性指定css样式,如果没写,则默认为active样式*/}
+                        <li><NavLink activeClassName="active" to="/home">首页</NavLink></li>
                         {/* 点击时跳转到/about路由 */}
-                        <li><Link to="/about">关于我们</Link></li>
+                        <li><NavLink activeClassName="active" to="/about">关于我们</NavLink></li>
                     </ul>
                 </nav>
             </div>
