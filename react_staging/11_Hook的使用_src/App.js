8a951d7636eb80cec24e9d2d892119c3a9f8ce97
Kenson <3049425262@qq.com>
2025-01-19 23:09:51 +0800

    useEffect的使用

--- react_staging/11_Hook的使用_src/App.js
+++ react_staging/11_Hook的使用_src/App.js
@@ -1,12 +1,19 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import Count from './compoents/Count'
 
-export default class App extends Component {
-  render() {
-    return (
-      <div>
-        <Count />
-      </div>
-    )
+export default function App() {
+  // 创建一个状态，用于控制Count组件的显示与隐藏
+  const [isShowCount, setIsShowCount] = useState(true);
+
+  // 卸载Count组件
+  const unmount = () => {
+    setIsShowCount(false);
   }
+
+  return (
+    <div>
+      {/* 当isShowCount为true时，Count组件才渲染，传入unmount方法，让Count组件调用unmount方法，控制Count组件的卸载 */}
+      {isShowCount && <Count unmount={unmount} />}
+    </div>
+  )
 }
--- react_staging/11_Hook的使用_src/compoents/Count/index.jsx
+++ react_staging/11_Hook的使用_src/compoents/Count/index.jsx
@@ -1,17 +1,51 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+
 
 /**
  * 函数式组件
  *  初次渲染调用一次，之后每次状态更新重新渲染组件调用一次
  */
-export default function Count() {
+// 接收unmount参数，用于在组件卸载时执行一些清理工作
+export default function Count({ unmount }) {
 
     /**
      * 使用useState创建一个count状态
      * 传入初始值0，初次渲染时会将初始值赋值给count
      * count为状态值，setCount为修改count状态值的函数
      */
-    const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0);
+
+    /**
+     * useEffect用于监听状态更新，初次渲染和状态更新都会执行
+     * 当count状态更新时，执行回调
+     * 第一个参数为回调函数
+     * 第二个参数为监听的依赖项
+     *  不传时，监听所有状态更新，当状态更新时都会执行回调
+     *  为空数组时相当于componentDidMount，只在首次渲染时执行
+     *  为依赖项数组时，监听数组中的状态更新，当数组中的状态更新时，执行回调
+     *  依赖项数组中为函数时，执行函数，返回一个数组，数组中的元素为依赖项
+     * 注意：只能监听setCount(preCount => preCount + 1)的更新，不能监听setCount(count + 1)的更新
+     */
+
+    useEffect(() => {
+        console.log('初次渲染');
+        const timer = setInterval(() => {
+            setCount(preCount => preCount + 1);
+        }, 500);
+        // 组件销毁前执行返回的回调函数
+        return () => {
+            console.log('组件销毁前');
+            clearInterval(timer);
+        }
+    }, [])
+
+
+    useEffect(() => {
+        console.log('count更新');
+        if (count > 5) {
+            setCount(0);
+        }
+    }, [count])
 
     const increment = () => {
         // 传入最新状态，修改count状态值
@@ -25,6 +59,7 @@ export default function Count() {
         <div>
             <h2>当前求和为{count}</h2>
             <button onClick={increment}>点我+1</button>
+            <button onClick={unmount}>卸载组件</button>
         </div>
     )
 }
