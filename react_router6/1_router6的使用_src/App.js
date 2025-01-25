0346594f2338b9ef4cb4f0891fad897d7cc68120
city118 <cityckc@qq.com>
2025-01-25 15:20:44 +0800

    Navigate重定向

--- react_router6/1_router6的使用_src/App.js
+++ react_router6/1_router6的使用_src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import TopBar from './compoents/TopBar'
 import Home from './pages/Home'
 import About from './pages/About'
@@ -10,7 +10,13 @@ function App() {
       <Routes>
         <Route path="/home" element={<Home />} />
         {/* caseSensitive为是否区分大小写,默认为false,写了只会就会区分大小写 */}
-        <Route path="/ABOUT" caseSensitive element={<About />} />
+        {/* <Route path="/ABOUT" caseSensitive element={<About />} /> */}
+        <Route path="/about" element={<About />} />
+        {/* 必须写重定向，否则会警告，重定向要使用组件Navigate
+        Navigate组件必须要有to属性,否则会报错
+        只要渲染Navigate组件，就会跳转到指定路由
+        以下的意思是当路径为/时，渲染Navigate组件 */}
+        <Route path="/" element={<Navigate to="/home"/>} />
       </Routes>
     </div>
   );
