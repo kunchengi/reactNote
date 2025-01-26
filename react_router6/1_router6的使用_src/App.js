567d313930ef55812370d72f4024f2a15bc23abd
city118 <cityckc@qq.com>
2025-01-26 17:04:36 +0800

    useInRouterContext

--- react_router6/1_router6的使用_src/App.js
+++ react_router6/1_router6的使用_src/App.js
@@ -1,10 +1,14 @@
 import TopBar from './compoents/TopBar'
-import { useRoutes } from 'react-router-dom'
+import { useRoutes, useInRouterContext } from 'react-router-dom'
 import routes from './routes'
 
 function App() {
   // 注册路由表
   const elements = useRoutes(routes)
+
+  // 判断是否在路由上下文环境，只要被BrowserRouter/hashRouter包裹，都在路由上下文环境，都返回true
+  // 返回一个布尔值
+  console.log(useInRouterContext());
 
   return (
     <div className="App">
