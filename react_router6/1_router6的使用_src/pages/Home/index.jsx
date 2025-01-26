5fb696815b078664eb907c369b9ef85b36bfa095
city118 <cityckc@qq.com>
2025-01-26 17:11:15 +0800

    useNavigationType获取当前的路由导航类型

--- react_router6/1_router6的使用_src/pages/Home/index.jsx
+++ react_router6/1_router6的使用_src/pages/Home/index.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigationType } from 'react-router-dom';
 import Sidebar from '../../compoents/Sidebar';
 import './Home.css';
 export default function Home() {
+    /**
+     * 返回当前的路由导航类型
+     *  POP: 点击浏览器的刷新按钮、浏览器直接打开当前路由
+     *  PUSH
+     *  REPLACE
+     */
+    console.log(useNavigationType());
+
     return (
         <div className="home-container">
             <Sidebar />
